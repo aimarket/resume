aa0b005d63f003df58e7860a3730d7a4f7432a29
Alex Torres <newbassvids@gmail.com>
2022-06-09 15:32:12 -0700

    blurred for marketing to public (TEMPORARY)

--- index.js
+++ index.js
@@ -1,63 +1,44 @@
 //create an async function to iterate through all the <p> tags in the documatn and 
 //change the css with a delay of 1 second after each element
 
-async function revealPTags() {
+async function revealTags() {
     // Returns a Promise that resolves after "ms" Milliseconds
     const timer = ms => new Promise(res => setTimeout(res, ms))
-    var pTags = document.getElementsByTagName('p');
-    await timer(4000);
-    for (var i = 0; i < pTags.length; i++) {
-        await timer(50);
-        pTags[i].style.color = 'aliceblue';
-    }
-}
-
-async function revealSpanTags() {
-    // Returns a Promise that resolves after "ms" Milliseconds
-    const timer = ms => new Promise(res => setTimeout(res, ms))
-    var spanTags = document.getElementsByTagName('span');
-    await timer(2000);
-    for (var i = 0; i < spanTags.length; i++) {
-        await timer(100);
-        spanTags[i].style.opacity = '1';
-    }
-}
-
-async function revealH3Tags() {
-    // Returns a Promise that resolves after "ms" Milliseconds
-    const timer = ms => new Promise(res => setTimeout(res, ms))
+    
     var h3Tags = document.getElementsByTagName('h3');
-    await timer(2000);
+    await timer(700);
     for (var i = 0; i < h3Tags.length; i++) {
-        await timer(200);
+        await timer(100);
         h3Tags[i].style.opacity = '1';
     }
-}
 
-async function revealH4Tags() {
-    // Returns a Promise that resolves after "ms" Milliseconds
-    const timer = ms => new Promise(res => setTimeout(res, ms))
     var h4Tags = document.getElementsByTagName('h4');
-    await timer(2000);
+    await timer(700);
     for (var i = 0; i < h4Tags.length; i++) {
-        await timer(200);
+        await timer(100);
         h4Tags[i].style.opacity = '1';
     }
-}
 
-async function revealH5Tags() {
-    // Returns a Promise that resolves after "ms" Milliseconds
-    const timer = ms => new Promise(res => setTimeout(res, ms))
     var h5Tags = document.getElementsByTagName('h5');
-    await timer(2000);
+    await timer(700);
     for (var i = 0; i < h5Tags.length; i++) {
-        await timer(200);
+        await timer(100);
         h5Tags[i].style.opacity = '1';
+    }
+
+    var spanTags = document.getElementsByTagName('span');
+    await timer(1000);
+    for (var i = 0; i < spanTags.length; i++) {
+        await timer(70);
+        spanTags[i].style.opacity = '1';
+    }
+
+    var pTags = document.getElementsByTagName('p');
+    await timer(1000);
+    for (var i = 0; i < pTags.length; i++) {
+        await timer(50);
+        pTags[i].style.color = 'transparent';
     }
 }
 
-revealH3Tags();
-revealH4Tags();
-revealH5Tags();
-revealSpanTags();
-revealPTags();
+revealTags();
